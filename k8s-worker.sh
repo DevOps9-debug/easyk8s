a3d1a292596ddfca0a2e2e351fefb6b5414a1e80
Dilbag Singh <113574343+DevOps9-debug@users.noreply.github.com>
2025-12-02 15:13:50 +0300

    Create k8s-worker.sh

---
+++ k8s-worker.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+echo "=== Updating system ==="
+apt update && apt upgrade -y
+
+echo "=== Enabling IP forwarding ==="
+cat <<EOF >/etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+sysctl --system
+
+echo "=== Disabling swap ==="
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+
+echo "=== Installing Docker + containerd ==="
+apt-get install -y ca-certificates curl gpg apt-transport-https
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
+https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" \
+> /etc/apt/sources.list.d/docker.list
+
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "=== Configuring containerd ==="
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+
+echo "=== Installing Kubernetes components ==="
+curl -fsSL https://pkgs.k8s.io/core:/stable:/vv1.33/deb/Release.key \
+| gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo \
+"deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] \
+https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /" \
+> /etc/apt/sources.list.d/kubernetes.list
+
+apt update
+apt install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+systemctl enable --now kubelet
+
+echo ""
+echo "=========================================="
+echo "  Worker Node Ready."
+echo "  Now RUN the join command from master:"
+echo ""
+echo "  kubeadm join <MASTER_IP>:6443 --token <TOKEN> --discovery-token-ca-cert-hash <HASH>"
+echo "=========================================="
